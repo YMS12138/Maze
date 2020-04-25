0dc5440574afbf1deda8cdfb8557370ed5e7df41
YMS <ymslucky@163.com>
2020-04-25 22:31:31 +0800

    complete! version - 1.0.0

---
+++ src/util/SwingUtil.java
@@ -0,0 +1,13 @@
+package util;
+
+import java.awt.*;
+
+/**
+ * 工具类
+ * @author YMS
+ */
+public class SwingUtil {
+    public static Dimension getScreen(){
+        return Toolkit.getDefaultToolkit().getScreenSize();
+    }
+}
